e3996c98cead768f85be2f7b1aa2a1b94cb7e9a3
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2024-12-23 11:35:05 +0300

    colors in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular11-material-variables';
+  palettes = ['primary', 'accent', 'warn'];
+  shades = [
+    '50',
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+    'A100',
+    'A200',
+    'A400',
+    'A700',
+    'contrast',
+    'default',
+    'lighter',
+    'darker',
+    'text',
+    'default-contrast',
+    'darker-contrast',
+    '50-contrast',
+    '100-contrast',
+    '200-contrast',
+    '300-contrast',
+    '400-contrast',
+    '500-contrast',
+    '600-contrast',
+    '700-contrast',
+    '800-contrast',
+    '900-contrast',
+    'A100-contrast',
+    'A200-contrast',
+    'A300-contrast',
+    'A700-contrast',
+  ];
 }
