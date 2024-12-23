96385618b092d69c9287f13a062d4e0e1382e338
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2024-12-23 20:33:21 +0300

    add color-palette.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ColorPaletteComponent } from './color-palette/color-palette.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorPaletteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/color-palette/color-palette.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-palette',
+  templateUrl: './color-palette.component.html',
+  styleUrls: ['./color-palette.component.scss']
+})
+export class ColorPaletteComponent {
+
+  palettes = ['primary', 'accent', 'warn'];
+  shades = [
+    '50',
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+    'A100',
+    'A200',
+    'A400',
+    'A700',
+    'contrast',
+    'default',
+    'lighter',
+    'darker',
+    // 'text',
+    // 'default-contrast',
+    // 'darker-contrast',
+    // '50-contrast',
+    // '100-contrast',
+    // '200-contrast',
+    // '300-contrast',
+    // '400-contrast',
+    // '500-contrast',
+    // '600-contrast',
+    // '700-contrast',
+    // '800-contrast',
+    // '900-contrast',
+    // 'A100-contrast',
+    // 'A200-contrast',
+    // 'A300-contrast',
+    // 'A700-contrast',
+  ];
+
+}
